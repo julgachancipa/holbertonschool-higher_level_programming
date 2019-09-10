b373d4e4558e70c344657ddb1af5e3d86842c070
julgachancipa <julianaga01@gmail.com>
2019-09-10 21:51:39 +0000

    v[4] - last digit - correct negative numbers output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ elif last == 0:
     s = "and is 0"
 else:
     s = "and is less than 6 and not 0"
-print ("Last digit of {:d} is {:d} {:s}".format(number, last, s))
+print("Last digit of {:d} is {:d} {:s}".format(number, last, s))

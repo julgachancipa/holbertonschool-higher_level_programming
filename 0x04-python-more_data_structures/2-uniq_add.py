bb3f100f260011e01c9dd300dc0c8cb4d54ca948
julgachancipa <julianaga01@gmail.com>
2019-09-19 17:09:16 -0500

    v[1] - add a list no repeated elements

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    add_list = set(my_list)
+    add_list = list(add_list)
     res = 0
-    for i in range(len(my_list)):
-        res += my_list[i]
+    for i in range(len(add_list)):
+        res += add_list[i]
     return res

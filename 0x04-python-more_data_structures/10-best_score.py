3f6c7a643a8f73e5cd55017cb2281563d39fe263
julgachancipa <julianaga01@gmail.com>
2019-09-19 18:47:54 -0500

    v[1] - (style) extra ;

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
-    best_key, best_val = "", 0;
+    best_key, best_val = "", 0
     for key, val in a_dictionary.items():
         if val > best_val:
             best_val = val

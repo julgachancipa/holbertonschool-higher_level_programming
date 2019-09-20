0ed4e34421910b5954a4f7d3644b291bb7febb69
julgachancipa <julianaga01@gmail.com>
2019-09-19 21:42:25 -0500

    v[0] -complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    del_keys = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            del_keys.append(key)
+    for i in del_keys:
+        del a_dictionary[i]
+    return a_dictionary

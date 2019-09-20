adab1e58443962ad9627ee6b46634b856bf4b787
julgachancipa <julianaga01@gmail.com>
2019-09-19 20:49:15 -0500

    v[0] - roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    last = 1
+    res = 0
+    for i in range(len(roman_string) - 1, -1, -1):
+        n = conv_rom_int(roman_string[i])
+        if last == -1:
+            break
+        if n == last:
+            res += n
+        elif n > last:
+            last = last_fun(last)
+            res += n
+        elif n < last:
+            last = last_fun(last)
+            res -= n
+    return res
+
+
+def conv_rom_int(n):
+    trl = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return trl[n]
+
+
+def last_fun(last):
+    l = {1: 5, 5: 10, 10: 50, 50: 100, 100: 500, 500: 1000, 1000: -1}
+    return l[last]

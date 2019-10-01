9b07698664aab821945b677d796ca4274d8b3ba1
julgachancipa <julianaga01@gmail.com>
2019-10-01 16:31:37 -0500

    [FEAUTURE] 1 version of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Write a class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+    """
+    def __init__(self, size=0):
+        self.__size = size

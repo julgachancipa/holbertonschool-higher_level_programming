d93b8e8d8e598178269face654a5bcbec0b5bc1b
julgachancipa <julianaga01@gmail.com>
2019-10-02 21:50:39 +0000

    [FEATURE] single linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -13,27 +13,39 @@ class Node:
         if type(value) is not int:
             raise TypeError('data must be an integer')
         self.__data = value
+
     @property
     def next_node(self):
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if value == None or type(value) != Node:
+        if value is not None and type(value) is not Node:
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
+
 class SinglyLinkedList:
+
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
         if self.__head is None:
-            self.__head = Node(value, None)
+            new_n = Node(value, None)
+            new_n.next_node = self.__head
+            self.__head = new_n
         else:
-            n = self.__head
-            while n != None:
-                if value > n.data:
-                    n = n.next_node
-                else:
-                    new(value, n.next_node)
+            new_n = Node(value)
+            new_n.data = value
+            new_n.next_node = self.__head
+            self.__head = new_n
+
+    def __str__(self):
+        current = self.__head
+        n_list = []
+        while current is not None:
+            n_list.append(str(current.data))
+            current = current.next_node
+        n_list.sort(key=int)
+        return ("\n".join(n_list))

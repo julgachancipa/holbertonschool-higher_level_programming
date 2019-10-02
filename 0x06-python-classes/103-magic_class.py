c6db2e33bf55f3906ecac6ee44e3fb1ac2b0780d
Emma Gachancipá <46653809+julgachancipa@users.noreply.github.com>
2019-10-02 08:49:25 -0500

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 class MagicClass:
     def __init__(self, radius):
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius

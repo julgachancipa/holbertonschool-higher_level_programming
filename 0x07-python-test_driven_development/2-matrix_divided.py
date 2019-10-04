7c794fba82858aac5d54ea91b8430da6bc7d2458
julgachancipa <julianaga01@gmail.com>
2019-10-04 23:55:07 +0000

    [FEATURE]  divides all elements of a matrix.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,21 +14,30 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
     if type(matrix) is not list:
         raise TypeError("matrix must be a matrix (list of lists)" +
                         " of integers/floats")
+    if len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists)" +
+                        " of integers/floats")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists)" +
+                            " of integers/floats")
     new_mtx = [x[:] for x in matrix]
     sz = len(matrix[0])
     for i in range(len(matrix)):
-        if type(matrix[i]) is not list:
+        if type(matrix[i]) is not list or len(matrix[0]) == 0:
             raise TypeError("matrix must be a matrix (list of lists)" +
                             " of integers/floats")
         if sz != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(matrix[i])):
-            if type(matrix[i][j]) is not int\
-               and type(matrix[i][j]) is not float:
+            if type(new_mtx[i][j]) is not int and \
+               type(new_mtx[i][j]) is not float:
                 raise TypeError("matrix must be a matrix (list of lists)" +
                                 " of integers/floats")
+
             new_mtx[i][j] = round(matrix[i][j] / div, 2)
     return new_mtx

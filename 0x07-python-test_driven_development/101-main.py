db5fc9521e633273ea6a675d94706816afcc6a51
julgachancipa <julianaga01@gmail.com>
2019-10-06 20:15:53 -0500

    [FEAUTURE] matrix mul

--- 0x07-python-test_driven_development/101-main.py
+++ 0x07-python-test_driven_development/101-main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
-print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+print(lazy_matrix_mul("a", "b"))
+print(lazy_matrix_mul(1, 8))
+print(lazy_matrix_mul(2, "a"))
+print(lazy_matrix_mul(2, True))
+print(lazy_matrix_mul(2, False))
+print(lazy_matrix_mul(True, False))

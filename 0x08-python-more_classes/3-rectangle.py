4ca467b697f226f0bbf9a32c9e184a9d05fb1f9d
julgachancipa <julianaga01@gmail.com>
2019-10-08 01:04:39 +0000

    [FEATURE] Rectangle V[3]

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    class Rectangle that defines a Rectangle 
+    - width
+    - height
+    - area
+    - perimeter
+    - __str__
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+    def __str__(self):
+        rec = []
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            rec.append("#" * self.__width)
+            rec.append("\n")
+        return ("".join(rec))
+        
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2

7b615a8369710b985208327ba1c76710f8aaf83d
julgachancipa <julianaga01@gmail.com>
2019-10-15 23:09:00 +0000

    [FEATURE] Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        super().area()
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)

a1da334d21e63885b1473cec2117caa1ce39bc7a
julgachancipa <julianaga01@gmail.com>
2019-10-16 20:12:15 +0000

    [FEATURE] write file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        charNum = myFile.write(text)
+    return charNum

83c07937eb711a5d12fff8301dc4c474423083c0
julgachancipa <julianaga01@gmail.com>
2019-10-17 02:34:27 +0000

    [FEATURE] hat defines a student by: (based on 11-student.py)

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_dic = {}
+        if attrs is not None:
+            for i in range(0, len(attrs)):
+                if attrs[i] in self.__dict__:
+                    my_dic[attrs[i]] = self.__dict__[attrs[i]]
+                    i += 1
+        else:
+            for key in self.__dict__:
+                my_dic[key] = self.__dict__[key]
+        return my_dic

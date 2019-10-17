215757cd12256179b2ea6ab71d4eb795dd2aeee0
julgachancipa <julianaga01@gmail.com>
2019-10-17 15:55:06 +0000

    [FIX] import inside copy

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """
-    -Public instance method: def area(self): that raises
-    an Exception with the message area() is not implemented
-    -Public instance method: def integer_validator(self, name, value):
-    that validates value
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

74b6048231efd6261211aec210ee92d236edf0d6
julgachancipa <julianaga01@gmail.com>
2019-10-17 00:53:00 +0000

    [FIX] returns the dictionary description with simple data structure

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def class_to_json(obj):
     flag = 0
-    my_str = '{'
+    my_dic = {}
     for key in obj.__dict__:
-        if flag:
-            my_str += ', '
-        flag = 1
-        my_str += '\''
-        my_str += key
-        my_str += '\': '
-        my_str += str(obj.__dict__[key])
-    my_str += '}'
-    return my_str
+        my_dic[key] = obj.__dict__[key]
+    return my_dic

120a9e41eac7938bbc0b3e6c4e60e53e32253cf7
julgachancipa <julianaga01@gmail.com>
2019-10-17 16:05:04 +0000

    [FIX] bad output

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@ def inherits_from(obj, a_class):
     of a class that inherited (directly or indirectly) from
     the specified class ; otherwise False.
     """
-    if issubclass(type(obj), a_class) and type(obj) is a_class:
-        return False
-    else:
+    if isinstance(obj, a_class) and not issubclass(a_class, type(obj)):
         return True
+    else:
+        return False

aacfd282b824023d0f44f4b7fd0de2523fb24bf8
julgachancipa <julianaga01@gmail.com>
2019-10-20 12:51:00 -0500

    [FEATURE] - __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,6 +23,11 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(self.__width * "#")
 
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".
+                format(self.id, self.__x, self.__y,
+                       self.__width, self.__height))
+
     @property
     def width(self):
         return self.__width

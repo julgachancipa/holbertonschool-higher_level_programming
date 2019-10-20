3d29e21f2df3d17d78accf95ebbdb460a3f0e937
julgachancipa <julianaga01@gmail.com>
2019-10-20 11:42:17 -0500

    [FIX] - class counter

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,9 +9,10 @@ class Base:
     first class Base
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id:
+        if id is not None:
             self.id = id
         else:
             self.__class__.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__class__.__nb_objects

e9d6bbf1c4ce214edb2a8adc861575c0e05c1a35
julgachancipa <julianaga01@gmail.com>
2019-10-20 23:00:57 -0500

    [FEATURE] - size getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,12 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y,
                        self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size

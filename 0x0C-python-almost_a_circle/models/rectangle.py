79557dc796e01412f49e4be7f60ba702118720b0
julgachancipa <julianaga01@gmail.com>
2019-10-20 23:02:17 -0500

    [FIX] change to super()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(Base):
     class Rectangle
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        Base.__init__(self, id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x

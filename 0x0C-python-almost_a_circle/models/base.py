0e99f2d76acdb55f29bec1eb3f15fd0daf9aaf9a
julgachancipa <julianaga01@gmail.com>
2019-10-21 21:20:57 +0000

    [FEATURE] save dict list to files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,18 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is not None and len(list_objs) > 0:
+            lst = []
+            for obj in list_objs:
+                json_dict = obj.to_dictionary()
+                lst.append(json_dict)
+            with open(cls.__name__ + ".json", mode='w',
+                      encoding='utf-8') as json_f:
+                json_f.write(cls.to_json_string(lst))
+        else:
+            with open(cls.__name__ + ".json", mode='w',
+                      encoding='utf-8') as json_f:
+                json_f.write(cls.to_json_string(list_objs))

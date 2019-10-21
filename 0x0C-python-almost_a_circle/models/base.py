c89740023d6d13c6c1fa204f1fb7b10f41f44519
julgachancipa <julianaga01@gmail.com>
2019-10-21 20:00:21 +0000

    [FEATURE] to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 first class Base
 """
+import json
 
 
 class Base:
@@ -16,3 +17,8 @@ class Base:
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)

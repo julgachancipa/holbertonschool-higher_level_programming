9ef62b0ea2979b5fb427034309f9d1a9aef8addb
julgachancipa <julianaga01@gmail.com>
2019-10-21 18:39:10 +0000

    [FEATURE] update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,24 @@ class Square(Rectangle):
     def size(self, size):
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        if args is not None and len(args) > 0:
+            for k in range(len(args)):
+                if k == 0:
+                    self.id = args[0]
+                elif k == 1:
+                    self.size = args[1]
+                elif k == 2:
+                    self.x = args[2]
+                elif k == 3:
+                    self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.width = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']

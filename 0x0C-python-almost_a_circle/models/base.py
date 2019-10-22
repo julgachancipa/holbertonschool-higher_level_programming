4f3549ac064468d9bdc8d755ebb766cffa98b520
julgachancipa <julianaga01@gmail.com>
2019-10-22 20:38:46 +0000

    [FIX] - or

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
         """
         to json m
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None and len(list_dictionaries) == 0:
             return '[]'
         return json.dumps(list_dictionaries)
 

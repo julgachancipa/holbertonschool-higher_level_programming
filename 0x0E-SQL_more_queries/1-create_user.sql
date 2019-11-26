15c2c93e8db04a8b9dffe5a0dbdc477518449a69
julgachancipa <julianaga01@gmail.com>
2019-11-26 11:57:40 -0500

    [FIX] - add flush

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
--- creates the MySQL server user user_0d_1
+-- creates user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant access
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';

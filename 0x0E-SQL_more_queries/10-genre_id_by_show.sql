755e36cb489ac2ef5e590986f1e2db90f7060be7
julgachancipa <julianaga01@gmail.com>
2019-11-26 19:52:19 -0500

    [FEATURE] - lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows
+SELECT DISTINCT
+	tv_shows.title, tv_show_genres.genre_id
+FROM
+	tv_shows
+JOIN tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

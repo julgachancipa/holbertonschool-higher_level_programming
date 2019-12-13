856add47d86cf940df2ff880f9e9e677efee9785
julgachancipa <julianaga01@gmail.com>
2019-12-13 19:04:45 +0000

    [Feature] - Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Get all states"""
+import sys
+import MySQLdb
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    mysql_usr = sys.argv[1]
+    mysql_psw = sys.argv[2]
+    db_name = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_usr,
+                           passwd=mysql_usr, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

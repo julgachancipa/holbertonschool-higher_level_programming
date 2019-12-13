7d919d4ca969dced894105ec0c6b2b68033a17c8
julgachancipa <julianaga01@gmail.com>
2019-12-13 19:51:19 +0000

    [Feature] - SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+import sys
+import MySQLdb
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+
+    mysql_usr = sys.argv[1]
+    mysql_psw = sys.argv[2]
+    db_name = sys.argv[3]
+    str_search = str(sys.argv[4])
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_usr,
+                           passwd=mysql_usr, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %(str_search)s ORDER BY id ASC"
+                , {'str_search': str_search})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

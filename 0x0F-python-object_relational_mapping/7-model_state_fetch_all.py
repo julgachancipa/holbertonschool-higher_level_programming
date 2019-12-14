83989d20a2c1513d224507fbd4ef73b3aa83bde2
julgachancipa <julianaga01@gmail.com>
2019-12-13 23:28:45 -0500

    [Fix] - Add port number

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     mysql_psw = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.forma\
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.forma\
 t(mysql_usr, mysql_psw, db_name), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 

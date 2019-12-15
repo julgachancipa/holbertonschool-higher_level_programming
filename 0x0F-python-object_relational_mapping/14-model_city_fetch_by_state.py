49de23e2d8cbf35d61fbb32c8f3f5a120476dedd
julgachancipa <julianaga01@gmail.com>
2019-12-14 23:46:50 -0500

    [Feature] - join tables

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Cities in state """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+
+    mysql_usr = sys.argv[1]
+    mysql_psw = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(mysql_usr, mysql_psw, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(State, City).join(City, State.id == City.state_id)
+    .order_by(City.id).all()
+    for s, c in q:
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
+    session.close()

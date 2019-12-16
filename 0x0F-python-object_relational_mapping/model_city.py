a25962f71086d8c05bc8c30aa277fdc821aeaaad
julgachancipa <julianaga01@gmail.com>
2019-12-15 21:43:41 -0500

    [Fix] - pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """class definition of city"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import relationship
+
 
 class City(Base):
     """City class"""

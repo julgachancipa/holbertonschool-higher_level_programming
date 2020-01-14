57139b42b419c35efa087b2992a0c21817f7e9db
julgachancipa <julianaga01@gmail.com>
2020-01-14 00:06:18 +0000

    [FEATURE] - post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""5-hbtn_header"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], email)
+    print(r.text)

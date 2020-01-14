66aedddce4f97f3119b23b23c7dc7d3d7fa0167f
julgachancipa <julianaga01@gmail.com>
2020-01-14 02:06:05 +0000

    [FIX] - exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     r = requests.post('http://0.0.0.0:5000/search_user', q)
     try:
         json_dict = r.json()
+        if len(json_dict) == 2:
+            print('[{}] {}'.format(json_dict.get('id'), json_dict.get('name')))
+        else:
+            print('No result')
     except:
         print('Not a valid JSON')
-    try:
-        print('[{}] {}'.format(json_dict['id'], json_dict['name']))
-    except:
-        print('No result')

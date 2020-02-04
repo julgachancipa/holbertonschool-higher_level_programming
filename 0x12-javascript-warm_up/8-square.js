574ca26a7b53a4ef67727b6fb6f5662781a6f6aa
julgachancipa <julianaga01@gmail.com>
2020-02-04 12:25:36 -0500

    [FIX] - style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 if (!process.argv[2] || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else if (process.argv[2] > 0) {
-  const x_str = 'X'.repeat(process.argv[2]);
+  const xStr = 'X'.repeat(process.argv[2]);
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log(x_str);
+    console.log(xStr);
   }
 }

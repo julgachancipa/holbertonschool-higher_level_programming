cad311703a3d855458915c4e534ef6ca0a6ae0e9
julgachancipa <julianaga01@gmail.com>
2020-02-04 14:13:25 -0500

    [FIX] - numeral sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,11 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   let allArg = process.argv.slice(2);
-  allArg.sort();
   allArg = allArg.map(Number);
   while (isNaN(allArg[allArg.length - 1])) {
     allArg.pop();
   }
+  allArg.sort(function (a, b) { return a - b; });
+  console.log(allArg);
   console.log(allArg[allArg.length - 2]);
 }

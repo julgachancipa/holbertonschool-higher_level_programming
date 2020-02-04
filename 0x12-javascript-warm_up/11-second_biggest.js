b7aef649b1e46d1ac50572052cac8f3f610939f2
julgachancipa <julianaga01@gmail.com>
2020-02-04 13:31:04 -0500

    [FIX] - Delete NaN

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,12 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const allArg = process.argv.slice(2);
+  let allArg = process.argv.slice(2);
   allArg.sort();
+  allArg = allArg.map(Number);
+  while (isNaN(allArg[allArg.length - 1])) {
+    allArg.pop();
+  }
+  console.log(allArg);
   console.log(allArg[allArg.length - 2]);
 }

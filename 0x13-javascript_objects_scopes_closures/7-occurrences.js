66eee4fc7e2f57c2d25b49564a6e9dc5caeb30a9
julgachancipa <julianaga01@gmail.com>
2020-02-10 23:50:24 -0500

    [FEATURE] Ocurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  list.forEach(item => {
+    if (item === searchElement) {
+      n += 1;
+    }
+  });
+  return n;
+};
